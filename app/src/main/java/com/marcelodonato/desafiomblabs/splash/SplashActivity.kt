387490af212f670ec9ae8b52c4f12fd9de2c6cc2
marcelodonato <62271815+MarceloDonato@users.noreply.github.com>
2022-06-09 19:59:56 -0300

    splashActivity

---
+++ app/src/main/java/com/marcelodonato/desafiomblabs/splash/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.marcelodonato.desafiomblabs.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.marcelodonato.desafiomblabs.R
+import com.marcelodonato.desafiomblabs.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed(
+            {
+                startWelcome()
+            }, 4000.toLong()
+        )
+    }
+
+
+    private fun startWelcome() {
+        val intent = Intent(baseContext, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
+    }
+
+}
