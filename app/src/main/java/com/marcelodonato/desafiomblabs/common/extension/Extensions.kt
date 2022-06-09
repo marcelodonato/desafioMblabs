ead1d083cadd7a0c3c3c0ef2ceb137198400b3ac
marcelodonato <62271815+MarceloDonato@users.noreply.github.com>
2022-06-09 15:15:32 -0300

    loginActivity

--- app/src/main/java/com/marcelodonato/desafiomblabs/common/extension/Extensions.kt
+++ app/src/main/java/com/marcelodonato/desafiomblabs/common/extension/Extensions.kt
@@ -1,2 +1,7 @@
 package com.marcelodonato.desafiomblabs.common.extension
 
+import android.widget.EditText
+
+
+fun EditText.getEditText() = text.toString().trim()
+fun EditText.validate() = text.toString().trim().isEmpty()
--- app/src/main/java/com/marcelodonato/desafiomblabs/login/LoginActivity.kt
+++ app/src/main/java/com/marcelodonato/desafiomblabs/login/LoginActivity.kt
@@ -1,12 +1,39 @@
 package com.marcelodonato.desafiomblabs.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.marcelodonato.desafiomblabs.R
+import com.marcelodonato.desafiomblabs.databinding.ActivityLoginBinding
+import com.marcelodonato.desafiomblabs.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        startBinding()
+        clickToRegister()
+
+
+    }
+
+    private fun startBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.root
+    }
+
+
+    private fun clickToRegister() {
+        binding.txtLogin.setOnClickListener {
+            goToRegisterActivity()
+        }
+    }
+
+
+    private fun goToRegisterActivity() {
+        val loginIntent = Intent(this, RegisterActivity::class.java)
+        startActivity(loginIntent)
     }
 }
